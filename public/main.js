74c53e0bc5a79201531cd373e2296a6651a334dd
Daniella McNair <daniella.mcnair495@gmail.com>
2023-11-15 21:02:09 -0600

    import/export data

--- public/main.js
+++ public/main.js
@@ -1,30 +1,7 @@
-// import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
-
-const students = [];
-const voldysArmy = []; // starts as an empty array
-
-const houses = [
-  {
-    house: 'gryffindor',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png'
-  },
-  {
-    house: 'slytherin',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png'
-  },
-  {
-    house: 'hufflepuff',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'
-  },
-  {
-    house: 'ravenclaw',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png'
-  }
-];
+import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
+import houses from '../utils/houses';
+import students from '../utils/students';
+import voldysArmy from '../utils/voldysArmy';
 
 const renderToDOM = (divId, content) => {
   const selectedDiv = document.querySelector(divId);
